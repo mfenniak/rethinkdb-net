1d6ec8d667d8b97c2dcf5d0e61b561a39a1c94cf
Karl Grzeszczak <karlgrz@gmail.com>
2013-04-12 17:04:18 -0500

    - forgot to add TestObject3.cs

---
+++ rethinkdb-net-test/TestObject3.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace RethinkDb.Test
+{
+	[DataContract]
+	public class TestObject3
+	{
+		[DataMember(Name = "id", EmitDefaultValue = false)]
+		public double Id;
+
+		[DataMember] 
+		public int Value_Int;
+
+		[DataMember] 
+		public int? Value_Int_Nullable;
+
+		[DataMember] 
+		public long Value_Long;
+
+		[DataMember] 
+		public long? Value_Long_Nullable;
+	}
+}
