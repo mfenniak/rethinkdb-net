a106c43bec6d57339678537d047dcc8fc6392e20
Dale Ragan <dale.ragan@moncai.com>
2013-04-23 17:36:57 -0400

    Returning an exit code from automated build. Fixes #85

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ build() {
 
 build_failed() {
     print_status "BUILD FAILED"
+    return 1
 }
 
 build_succeeded() {
@@ -22,6 +23,21 @@ build_succeeded() {
 run_tests() {
     RUNNER_PATH="packages/NUnit.Runners.2.6.1/tools"
     mono ${RUNNER_PATH}/nunit-console.exe rethinkdb-net-test/bin/Debug/rethinkdb-net-test.dll
+    
+    if [[ $? != 0 ]] ; then
+        tests_failed
+    else
+        tests_passed
+    fi
+}
+
+tests_failed() {
+    print_status "TESTS FAILED"
+    return 2
+}
+
+tests_passed() {
+    print_status "TESTS PASSED"
 }
 
 print_status() {
@@ -31,3 +47,7 @@ print_status() {
 }
 
 build
+
+EXIT_CODE=$?
+echo "Exit [$EXIT_CODE]"
+exit $EXIT_CODE
