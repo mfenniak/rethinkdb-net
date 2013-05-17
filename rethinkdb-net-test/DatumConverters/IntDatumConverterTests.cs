a872f5ffa616ba5d2b9dee08c2762b543f564346
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2013-05-17 10:41:30 -0600

    Namespace DatumConverters unit tests

--- rethinkdb-net-test/DatumConverters/IntDatumConverterTests.cs
+++ rethinkdb-net-test/DatumConverters/IntDatumConverterTests.cs
@@ -1,8 +1,7 @@
 using System;
 using NUnit.Framework;
-using ProtoBuf;
 
-namespace RethinkDb.Test
+namespace RethinkDb.Test.DatumConverters
 {
     [TestFixture]
     public class IntDatumConverterTests
--- rethinkdb-net-test/DatumConverters/NullableDecimalDatumConverterTests.cs
+++ rethinkdb-net-test/DatumConverters/NullableDecimalDatumConverterTests.cs
@@ -1,7 +1,7 @@
 using System;
 using NUnit.Framework;
 
-namespace RethinkDb.Test
+namespace RethinkDb.Test.DatumConverters
 {
     [TestFixture]
     public class NullableDecimalDatumConverterTests
