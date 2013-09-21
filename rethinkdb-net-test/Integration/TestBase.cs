df2ea86e6fd884800c7fb9298cd5f9fba67a918f
Greg Lincoln <greg@greglincoln.com>
2013-09-21 10:01:40 -0400

    first pass at running rethinkdb from the integration tests instead of build script

--- rethinkdb-net-test/Integration/TestBase.cs
+++ rethinkdb-net-test/Integration/TestBase.cs
@@ -5,18 +5,24 @@ using System.Net;
 using System.Linq;
 using System.Threading.Tasks;
 using RethinkDb.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace RethinkDb.Test.Integration
 {
     public class TestBase
     {
         protected IConnection connection;
+        protected Process rethinkProcess;
 
         [TestFixtureSetUp]
         public virtual void TestFixtureSetUp()
         {
             try
             {
+                StartRethinkDb();
                 DoTestFixtureSetUp().Wait();
             }
             catch (Exception e)
@@ -44,9 +50,31 @@ namespace RethinkDb.Test.Integration
             }
         }
 
+        private void StartRethinkDb()
+        {
+            var solutionPath = Path.GetDirectoryName((new System.Uri(Assembly.GetExecutingAssembly().CodeBase)).AbsolutePath);
+
+            var dbPath = Path.Combine(solutionPath, "tmp/rethink");
+
+            if(Directory.Exists(dbPath))
+               Directory.Delete(dbPath, true);
+
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = "/usr/local/bin/rethinkdb",
+                Arguments = "-d " + dbPath + " --driver-port 55558",
+                UseShellExecute = false
+            };
+            rethinkProcess = Process.Start(processInfo);
+
+            // wait for it to start up. Is there a better way to do this?
+            Thread.Sleep(1000);
+        }
+
         [TestFixtureTearDown]
         public virtual void TestFixtureTearDown()
         {
+            rethinkProcess.Kill();
         }
     }
 }
