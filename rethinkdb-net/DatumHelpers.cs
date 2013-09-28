bebf359a6b5683578c147841ab131168122b89b0
Greg Lincoln <greg@greglincoln.com>
2013-09-28 10:14:48 -0400

    Add AnonymousTypeConverter tests.

--- rethinkdb-net/DatumHelpers.cs
+++ rethinkdb-net/DatumHelpers.cs
@@ -1,5 +1,7 @@
 using System;
 using RethinkDb.Spec;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RethinkDb
 {
@@ -40,6 +42,18 @@ namespace RethinkDb
         public static Datum ToDatum(this int num)
         {
             return ((double)num).ToDatum();
+        }
+
+        public static Datum ToDatum(this Dictionary<string, string> kvPairs)
+        {
+            var dtm = new Datum { type = Datum.DatumType.R_OBJECT };
+            dtm.r_object.AddRange(kvPairs.Select(DatumHelpers.ToDatum));
+            return dtm;
+        }
+
+        public static Datum.AssocPair ToDatum(this KeyValuePair<string, string> kvPair)
+        {
+            return new Datum.AssocPair() { key = kvPair.Key, val = kvPair.Value.ToDatum() };
         }
     }
 }
